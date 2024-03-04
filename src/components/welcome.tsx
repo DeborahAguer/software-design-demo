42acecabb82cbf9d9ae528d465e25c72562d7c03
DeborahAguer <61664374+DeborahAguer@users.noreply.github.com>
2024-03-04 14:10:31 -0400

    Update welcome.tsx
    I change the file to make the make more responsive

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,12 +1,12 @@
-export default function Welcome(){
-    return (
-        <main>
-      <div className="flex justify-between p-4 text-white font-medium">
-        <button className="px-4 py-2 bg-blue-500 rounded-full">
+export default function Welcome() {
+  return (
+    <main>
+      <div className="flex flex-col sm:flex-row justify-between p-4 text-white font-medium">
+        <button className="mb-4 sm:mb-0 sm:mr-4 px-4 py-2 bg-blue-500 rounded-full">
           View Menu
         </button>
-        <div className="space-x-4">
-          <button className="px-4 py-2 bg-green-500  rounded-full">
+        <div className="space-y-4 sm:space-y-0 sm:space-x-4 flex flex-col sm:flex-row">
+          <button className="px-4 py-2 bg-green-500 rounded-full">
             Sign In
           </button>
           <button className="px-4 py-2 bg-red-500 rounded-full">
@@ -20,11 +20,10 @@ export default function Welcome(){
       <div className="text-center my-8">
         <h1 className="text-4xl font-semibold mb-4">Welcome to Our Club!</h1>
         <p className="text-lg text-gray-700">
-          Welcome to our club app where you can order delicious meals
-
+          Welcome to our club app where you can order delicious meals.
         </p>
       </div>
       {/* Menu component can be added here */}
     </main>
-    )
+  );
 }
